da92feb64366c9ac92c9f78deab77801ede7b083
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-18 10:33:56 +0900

    パスワード認証用にspec_helperを変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 require 'serverspec'
 require 'pathname'
 require 'net/ssh'
+require 'highline/import'
 
 include Serverspec::Helper::Ssh
 include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
   if ENV['ASK_SUDO_PASSWORD']
-    require 'highline/import'
     c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
   else
     c.sudo_password = ENV['SUDO_PASSWORD']
@@ -25,6 +25,12 @@ RSpec.configure do |c|
       c.host  = host
       options = Net::SSH::Config.for(c.host)
       user    = options[:user] || Etc.getlogin
+
+      if ENV['ASK_LOGIN_PASSWORD']
+        options[:password] = ask("\nEnter login password: ") { |q| q.echo = false}
+      else
+        options[:password] = ENV['LOGIN_PASSWORD']
+      end
       
       c.ssh   = Net::SSH.start(c.host, user, options)
     end
