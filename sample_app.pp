7f68cc2af76ffa4d85c4eab93d9c83f95b75cc5e
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-19 17:06:52 +0900

    必要なパッケージのインストールとユーザ・グループの作成

--- sample_app.pp
+++ sample_app.pp
@@ -1,13 +1,43 @@
-package { 'nginx':
+$packages = [
+  'zsh',
+  'mysql-server',
+  'mysql-devel',
+  'nginx',
+  'libyaml',
+  'libyaml-devel',
+  'zlib',
+  'zlib-devel',
+  'readline',
+  'readline-devel',
+  'openssl',
+  'openssl-devel',
+  'libxml2',
+  'libxml2-devel',
+  'libxslt',
+  'libxslt-devel',
+]
+
+package { $packages:
   ensure => installed,
+}
+
+user { 'app001':
+  ensure => present,
+  comment => 'app001',
+  home => '/home/app001',
+  managehome => true,
+  shell => '/bin/bash',
+  uid => 1000,
+  gid => 'app001',
+}
+
+group { 'app001':
+  ensure => present,
+  gid => 1000,
 }
 
 service { 'nginx':
   enable => true,
   ensure => running,
   hasrestart => true,
-}
-
-package { 'mysql-server':
-  ensure => installed,
 }
