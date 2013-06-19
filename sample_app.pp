cf96713f4bec79308d700cd5bc4b6e7e8702a53e
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-19 16:50:11 +0900

    nginx, mysql-serverの導入をマニフェストに追記

--- sample_app.pp
+++ sample_app.pp
@@ -1,1 +1,13 @@
-notice("Hello, World!")
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  enable => true,
+  ensure => running,
+  hasrestart => true,
+}
+
+package { 'mysql-server':
+  ensure => installed,
+}
