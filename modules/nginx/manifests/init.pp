28e17ab131c443db5bd7316c212b7ef5ebb17ec1
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-01 16:20:27 +0900

    インデントの調整

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -3,7 +3,7 @@ class nginx () {
   include nginx::config
   include nginx::service
 
-  Class['nginx::install']
+     Class['nginx::install']
   -> Class['nginx::config']
   ~> Class['nginx::service']
 }
