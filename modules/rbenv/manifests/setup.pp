c01dcedf9169632f3e044f4a7c02629d11cb93cf
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-05 11:46:56 +0900

    rbenv内のexec pathをまとめた

--- modules/rbenv/manifests/setup.pp
+++ modules/rbenv/manifests/setup.pp
@@ -1,16 +1,19 @@
 class rbenv::setup {
   $ruby_version = "2.0.0-p195"
+
+  Exec {
+    path =>  ['/usr/local/rbenv/shims', '/usr/local/rbenv/bin', '/bin', '/usr/bin'],
+  }
+
   exec { "use ruby${ruby_version}":
     user        => 'app',
     environment => ['RBENV_ROOT="/usr/local/rbenv"'],
-    path        => ['/bin', '/usr/bin', '/usr/local/rbenv/bin'],
     command     => "echo '${ruby_version}' > /usr/local/rbenv/version",
     unless      => "test `cat /usr/local/rbenv/version` == '${ruby_version}'",
   } 
   
   exec { 'bundler':
     environment => ['RBENV_ROOT="/usr/local/rbenv"'],
-    path        => ['/usr/local/rbenv/shims', '/usr/local/rbenv/bin', '/bin', '/usr/bin'],
     command     => "sh -c 'source /etc/profile.d/rbenv.sh; gem install bundler; rbenv rehash'",
     unless      => "test -x /usr/local/rbenv/shims/bundle",
     require     => Exec["use ruby${ruby_version}"],
