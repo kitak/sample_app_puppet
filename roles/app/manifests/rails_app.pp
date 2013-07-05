2d09111d484e603dab111a5ef6d5c5d81938dae0
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-05 12:46:58 +0900

    create /var/www resource

--- roles/app/manifests/rails_app.pp
+++ roles/app/manifests/rails_app.pp
@@ -8,11 +8,19 @@ class app::rails_app {
     content => template('app/unicorn_ini.sh'),
   }
 
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+
   file { '/var/www/rails':
     ensure => directory,
     owner => 'app',
     group => 'app',
-    mode => '0755'
+    mode => '0755', 
+    require => File['/var/www'],
   }
 
 } 
