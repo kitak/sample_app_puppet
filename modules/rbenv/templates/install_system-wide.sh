f1885acb23ea9a32f939a6c51b8012fd82387b89
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-06 12:47:21 +0900

    fix permission

--- modules/rbenv/templates/install_system-wide.sh
+++ modules/rbenv/templates/install_system-wide.sh
@@ -4,12 +4,12 @@ cd /usr/local
 git clone git://github.com/sstephenson/rbenv.git rbenv
 mkdir rbenv/shims rbenv/versions
 chgrp -R $MY_GROUP rbenv
-chmod -R g+rwxXs rbenv
+chmod -R g+rwxX rbenv
  
 git clone git://github.com/sstephenson/ruby-build.git ruby-build
 ./ruby-build/install.sh
 chgrp -R $MY_GROUP ruby-build
-chmod -R g+rwxs ruby-build 
+chmod -R g+rwx ruby-build 
  
 echo 'export RBENV_ROOT="/usr/local/rbenv"'     >> /etc/profile.d/rbenv.sh
 echo 'export PATH="/usr/local/rbenv/bin:$PATH"' >> /etc/profile.d/rbenv.sh
