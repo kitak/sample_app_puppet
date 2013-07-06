1fa47c3e3eb5fb0be09e4d93c93911250a4011bc
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-06 15:12:58 +0900

    fix typo

--- modules/monit/manifests/service.pp
+++ modules/monit/manifests/service.pp
@@ -1,11 +1,11 @@
 class monit::service {
-  exec { 'start monit'
+  exec { 'start monit':
     path    => ['/sbin', '/bin'],
     command => 'initctl start monit',
-    unless  => 'initctl status monit | grep running'
+    unless  => 'initctl status monit | grep running',
   }
 
-  exec { 'reload monit'
+  exec { 'reload monit':
     path    => ['/sbin', '/bin'],
     command => 'initctl reload monit',
     onlyif  => 'initctl status monit | grep running',
