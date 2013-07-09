b287a87e2802f943b71e1b5d68974205a71cf758
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-09 11:51:00 +0900

    add proxy role's init.pp

---
+++ roles/proxy/manifests/init.pp
@@ -0,0 +1,16 @@
+class proxy {
+  include ::nginx
+  include app::nginx::config
+
+  $etc_packages = [
+    'wget',
+    'zsh',
+    'nc',
+  ]
+  package { $etc_packages:
+    ensure => installed,
+  }
+
+     Class['app::nginx::config']
+  ~> Class['::nginx::service']
+}
