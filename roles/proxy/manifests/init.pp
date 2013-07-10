1d532b0bf14e6f1e39555e89298b1bdbebdb4bc4
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-10 14:14:44 +0900

    proxyで静的コンテンツを返すためにmanifestの修正

--- roles/proxy/manifests/init.pp
+++ roles/proxy/manifests/init.pp
@@ -1,5 +1,8 @@
 class proxy {
   include ::nginx
+  include ::rbenv
+  include app::user_group
+  include app::rails_app
   include proxy::nginx::config
 
   $etc_packages = [
@@ -10,6 +13,10 @@ class proxy {
   package { $etc_packages:
     ensure => installed,
   }
+
+     Class['app::user_group']
+  -> Class['app::rails_app']
+  -> Class['::rbenv::install']
 
      Class['proxy::nginx::config']
   ~> Class['::nginx::service']
