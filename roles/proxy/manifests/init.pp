9d23a3cc85419a27b0baad7337416e9e21d30880
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-10 14:44:05 +0900

    proxy role内でsample_appを切り出し

--- roles/proxy/manifests/init.pp
+++ roles/proxy/manifests/init.pp
@@ -2,22 +2,8 @@ class proxy {
   include ::nginx
   include ::rbenv
   include app::user_group
+  include proxy::rails_app 
   include proxy::nginx::config
-
-  file { '/var/www':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-
-  file { '/var/www/rails':
-    ensure => directory,
-    owner => 'app',
-    group => 'app',
-    mode => '0755', 
-    require => File['/var/www'],
-  }
 
   $etc_packages = [
     'wget',
---
+++ roles/proxy/manifests/rails_app.pp
@@ -0,0 +1,20 @@
+class proxy::rails_app {
+  package { "mysql-devel":
+    ensure => installed,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+
+  file { '/var/www/rails':
+    ensure => directory,
+    owner => 'app',
+    group => 'app',
+    mode => '0755', 
+    require => File['/var/www'],
+  }
+}
