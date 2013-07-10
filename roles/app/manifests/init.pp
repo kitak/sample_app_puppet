67931e74c23650049a13ae0b4f6af95d47fef741
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-10 14:30:07 +0900

    app roleからnginxを削除

--- roles/app/manifests/init.pp
+++ roles/app/manifests/init.pp
@@ -1,10 +1,8 @@
 class app {
-  include ::nginx
   include ::rbenv
   include ::monit
   include app::user_group
   include app::rails_app
-  include app::nginx::config
   include app::monit::config
 
   $etc_packages = [
@@ -19,9 +17,6 @@ class app {
      Class['app::user_group']
   -> Class['app::rails_app']
   -> Class['::rbenv::install']
-
-     Class['app::nginx::config']
-  ~> Class['::nginx::service']
 
      Class['app::monit::config']
   ~> Class['::monit::service']
--- roles/app/manifests/nginx/config.pp
+++
@@ -1,9 +0,0 @@
-class app::nginx::config { 
-  file { '/etc/nginx/conf.d/unicorn.conf':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('app/nginx/unicorn.conf'),
-  }
-}
