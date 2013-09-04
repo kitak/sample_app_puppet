f3020921f34b330f56c8ba250ea33c2d106f4de4
Keisuke KITA <kei.kita2501@gmail.com>
2013-09-04 09:32:19 +0900

    勘違いしていたので入れ替え

--- vagrant_app_setup.sh
+++ vagrant_app_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [-d /tmp/sample_app_puppet]; then
-  sudo yum -y install git
-  sudo gem install puppet --no-ri --no-rdoc
+if [ -d /tmp/sample_app_puppet ]; then
   cd /tmp/sample_app_puppet
   git pull origin master
 else
+  sudo yum -y install git
+  sudo gem install puppet --no-ri --no-rdoc
   cd /tmp
   git clone https://github.com/kitak/sample_app_puppet.git
   cd ./sample_app_puppet
